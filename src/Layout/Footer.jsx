eedc5941ac44832e9dea23b78c736c5cfe986229
Subhanuddin031 <Subhanuddin50@hotmail.com>
2023-10-07 22:46:39 +0500

    First ever react

---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <h1>This is the footer of the footer file</h1>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <h1>This is the header of the header file</h1>
+    </>
+  )
+}
+
+export default Header
